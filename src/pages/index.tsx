c5ca001a85027dc7ef795b457e415cf138d8620c
ankhmorporklord <ankhmorporklord@gmail.com>
2025-01-20 19:33:05 +0100

    feat: data updated and styles updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,94 +1,14 @@
 import * as React from "react";
-import { graphql, HeadFC, PageProps } from "gatsby";
-import "../library.scss";
-import Header from "../components/Header/Header";
-import Footer from "../components/Footer/Footer";
-import { Data } from "../../gatsby-config";
-import Section, { SectionType } from "../components/Section/Section";
 import "./index.scss";
-import { createContext, useState } from "react";
+import Dashboard from "./Dashboard";
+import { ThemeProvider } from "../providers/useThemeProvider";
 
-type themeType = "dark" | "light";
-type themeDetailType = "human" | "tech" | undefined;
-
-interface themeContextType {
-  theme: themeType;
-  themeDetail?: themeDetailType;
-  setTheme: (newTheme: themeType) => void;
-  setThemeDetail: (newThemeDetail: themeDetailType) => void;
-}
-
-const initialContext: themeContextType = {
-  theme: "dark",
-  themeDetail: undefined,
-  setTheme: () => {},
-  setThemeDetail: () => {},
-};
-
-export const ThemeContext = createContext(initialContext);
-
-const IndexPage: React.FC<PageProps> = ({
-  data,
-}: {
-  data: Data;
-  sections: SectionType[];
-}) => {
-  const [theme, setTheme] = useState<themeType>("dark");
-  const [themeDetail, setThemeDetail] = useState<themeDetailType>(undefined);
-
-  const {
-    site: {
-      siteMetadata: { header, sections },
-    },
-  } = data || {};
-  // add Header to the second position of the sections
-
-  const sectionsAndHeader: SectionType[] = sections.toSpliced(1, 0, {
-    title: "header",
-    type: "human",
-    info: [],
-  });
-
+const IndexPage = () => {
   return (
-    <ThemeContext.Provider
-      value={theme}
-      setTheme={setTheme}
-      themeDetail={themeDetail}
-      setThemeDetail={setThemeDetail}
-    >
-      <div className={`index-layout ${theme}`}>
-        <div className="index-sections">
-          {(sectionsAndHeader || []).map((section: SectionType) => {
-            if (section.title === "header") return <Header {...header} />;
-            return <Section {...section} />;
-          })}
-        </div>
-        <Footer />
-      </div>
-    </ThemeContext.Provider>
+    <ThemeProvider>
+      <Dashboard />
+    </ThemeProvider>
   );
 };
 
 export default IndexPage;
-
-export const Head: HeadFC = () => <title>Home Page</title>;
-
-export const query = graphql`
-  query {
-    site {
-      siteMetadata {
-        header {
-          name
-          position
-        }
-        sections {
-          title
-          info {
-            text
-            prefix
-          }
-        }
-      }
-    }
-  }
-`;
