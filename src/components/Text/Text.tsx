343942079f9abaeb3c07e54dd2ed44b3ad292bf9
ankhmorporklord <ankhmorporklord@gmail.com>
2025-01-24 20:40:24 +0100

    feat: text adapted to dark/light theme

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,53 +1,66 @@
 import classes from "classnames";
 import React, { useContext } from "react";
 import "./Text.scss";
-import { ThemeContext } from "../../pages";
+import { getReversedTheme } from "../../utils/theme";
+import useThemeProvider, { themeType } from "../../providers/useThemeProvider";
 
 export type TextType = "header" | "title" | "subtitle" | "text";
 
 interface TextProps {
   prefix?: string;
+  themeFromParent?: themeType;
   text?: string;
   type?: TextType;
   showSquareWrapper?: boolean;
   showSquareWrapperLine?: boolean;
+  onClick?: () => void;
 }
 
 const Text = ({
   prefix,
   type = "text",
   text = "",
+  themeFromParent,
   showSquareWrapper,
   showSquareWrapperLine,
+  onClick,
 }: TextProps) => {
-  const { theme } = useContext(ThemeContext);
+  const { theme } = useThemeProvider();
 
-  const themeReversed = theme === "dark" ? "light" : "dark";
+  console.log("text - theme: ", theme);
+
+  const themeReversed = themeFromParent || getReversedTheme(theme);
 
   const showWrapper = type === "title" || showSquareWrapper;
   return (
-    <>
-      <div className={classes(showWrapper && "wrapper-layout", themeReversed)}>
-        {prefix && (
-          <span className={classes(type, "text-width", "bold", themeReversed)}>
-            {prefix}
-          </span>
-        )}
-        <span
-          className={classes(
-            type,
-            "text-width",
-            showWrapper && `wrapper-square ${themeReversed}`,
-          )}
-        >
-          {text}
+    <div
+      className={classes(
+        showWrapper && "wrapper-layout",
+        themeReversed,
+        Boolean(onClick) && "clickable",
+        `padding-${type}`,
+      )}
+      onClick={onClick}
+    >
+      {prefix && (
+        <span className={classes(type, "text-width", "bold", themeReversed)}>
+          {prefix}
         </span>
-
-        {showWrapper && showSquareWrapperLine !== false && (
-          <div className={classes(`wrapper-line ${themeReversed}`)}></div>
+      )}
+      <span
+        className={classes(
+          type,
+          "text-width",
+          showWrapper && `wrapper-square ${themeReversed}`,
         )}
-      </div>
-    </>
+      >
+        {text}
+      </span>
+
+      {showWrapper && showSquareWrapperLine !== false && (
+        <div className={classes(`wrapper-line ${themeReversed}`)}></div>
+      )}
+    </div>
   );
 };
 
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+import { HeadFC } from "gatsby";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import Section, { SectionType } from "../components/Section/Section";
+import "./index.scss";
+import data from "../../data.json";
+import useThemeProvider from "../providers/useThemeProvider";
+import { useContext } from "react";
+
+const Dashboard = () => {
+  const { theme } = useThemeProvider();
+  // add Header to the second position of the sections
+
+  const { sections, header } = data;
+
+  // @ts-ignore
+  const sectionsAndHeader: SectionType[] = sections.toSpliced(1, 0, {
+    title: "header",
+    type: "human",
+    info: [],
+  });
+
+  return (
+    <div className={`index-layout ${theme}`} key="header">
+      <div className="index-sections">
+        {(sectionsAndHeader || []).map((section: SectionType) => {
+          if (section.title === "header") return <Header {...header} />;
+          return (
+            <div className="index-sections-child" key={section.title}>
+              <Section {...section} />
+            </div>
+          );
+        })}
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Dashboard;
+
+export const DashboardHead: HeadFC = () => <title>Dashboard</title>;
+/*
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        header {
+          name
+          position
+        }
+        sections {
+          title
+          info {
+            text
+            prefix
+          }
+        }
+      }
+    }
+  }
+`;*/
